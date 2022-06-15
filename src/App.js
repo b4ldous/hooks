8394e8685ba82bbeedba4d165d2effe5ab0aad08
b4ldous <baldous@protonmail.com>
2022-06-14 21:22:48 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Container from "@mui/system/Container";
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
