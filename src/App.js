dfbc700385614a4238eab6a5b109993ba061c9a0
b4ldous <baldous@protonmail.com>
2022-06-15 15:38:22 -0500

    adding an item to a list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-function App() {
-  return <div>This is gonna be the template with react hooks</div>;
-}
+import React from "react";
+import { v4 as uuidv4 } from 'uuid';
+const initialList = [
+  {
+    id: "a",
+    name: "Robin",
+  },
+  {
+    id: "b",
+    name: "Dennis",
+  },
+];
+
+const App = () => {
+  const [list, setList] = React.useState(initialList);
+  const [name, setName] = React.useState('');
+
+  function handleChange(event) {
+    // track input field's state
+    setName(event.target.value);
+  }
+
+  function handleAdd() {
+    // add item
+    const newList = list.concat({ name, id: uuidv4() });
+
+    setList(newList);
+    setName('');
+  }
+  return (
+    <div>
+      <div>
+        <input type="text" value={name} onChange={handleChange} />
+        <button type="button" onClick={handleAdd}>
+          Add
+        </button>
+      </div>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
