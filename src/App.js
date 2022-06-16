a5ec6543969db756964d7b92322704ba668cb734
b4ldous <baldous@protonmail.com>
2022-06-15 20:40:24 -0500

    remove an item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
-function App() {
-  return <div>This is gonna be the template with react hooks</div>;
-}
+import React, {useState} from "react";
+const initialList = [
+  {
+    id: "a",
+    firstname: "Robin",
+    lastname: "Wieruch",
+    year: 1988,
+  },
+  {
+    id: "b",
+    firstname: "Dave",
+    lastname: "Davidds",
+    year: 1990,
+  },
+];
+
+const App = () => {
+
+  const [list, setList]= useState(initialList);
+  function handleRemove(id) {
+    console.log(id);
+    // remove item
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
+  }
+
+  return (
+    <div>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>
+            <span>{item.firstname}</span>
+            <span>{item.lastname}</span>
+            <span>{item.year}</span>
+            <button type="button" onClick={() => handleRemove(item.id)}>
+            Remove
+          </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
